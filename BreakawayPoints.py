ad640971b1ab2337752adcc764e73d5c07ff61e8
makanaraini <danielraini871@gmail.com>
2024-09-30 09:06:31 +0300

    Parameterization: Allow the transfer function parameters to be passed as arguments.

--- BreakawayPoints.py
+++ BreakawayPoints.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import control as ctrl
 
-def plot_root_locus(numerator, denominator):
+def plot_root_locus(numerator, denominator, xlim=(-10, 2), ylim=(-10, 10)):
     """
     This function plots the root locus of a system, along with poles, zeros, 
     asymptotes, and centroid.
@@ -10,6 +10,8 @@ def plot_root_locus(numerator, denominator):
     Parameters:
     numerator (list): The numerator coefficients of the transfer function.
     denominator (list): The denominator coefficients of the transfer function.
+    xlim (tuple): Limits for the x-axis of the plot.
+    ylim (tuple): Limits for the y-axis of the plot.
     """
     
     # Create the transfer function
@@ -38,7 +40,7 @@ def plot_root_locus(numerator, denominator):
         angles = [(2*i + 1) * np.pi / n_diff for i in range(n_diff)]  # Asymptote angles
 
         for angle in angles:
-            x = np.linspace(-10, 10, 100)  # Generate points for the asymptote lines
+            x = np.linspace(xlim[0], xlim[1], 100)  # Generate points for the asymptote lines
             plt.plot(x, np.tan(angle) * (x - centroid), 'k--', lw=1, label="Asymptotes")
 
         plt.scatter([centroid], [0], color='green', marker='P', s=100, label='Centroid')
@@ -55,7 +57,8 @@ def plot_root_locus(numerator, denominator):
     plt.axvline(0, color='black', lw=1)  # Imaginary axis line
     plt.grid(True)
     plt.legend(loc='best')
-    plt.xlim([-10, 2])  # Adjust the x-axis limits for better visibility
+    plt.xlim(xlim)  # Adjust the x-axis limits based on function parameters
+    plt.ylim(ylim)  # Adjust the y-axis limits based on function parameters
 
     # Show the plot
     plt.show()
@@ -64,4 +67,4 @@ def plot_root_locus(numerator, denominator):
 # Example usage:
 numerator = [1]  # Numerator (K, which will vary)
 denominator = [1, 5, 6]  # Denominator (s^2 + 5s + 6)
-plot_root_locus(numerator, denominator)
+plot_root_locus(numerator, denominator, xlim=(-10, 2), ylim=(-10, 10))
